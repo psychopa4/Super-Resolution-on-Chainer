43eede599295ef53c37ddd2e21a30f09699fc760
psychopa4 <494459956@qq.com>
2018-10-10 19:11:02 +0800

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+import os
+from os.path import join, exists
+import glob
+import six
+import numpy as np
+import math
+import cv2
+import random
+from PIL import Image
+import imageio
+import scipy.misc
+from chainer.dataset import dataset_mixin
+
+def randomcrop(img, size=128):
+    h,w,c=img.shape
+    x = random.randint(0, h - size)
+    y = random.randint(0, w - size)
+
+    return img[x:x+size,y:y+size,:]
+
+def augumentation(img):
+    flipx=random.randint(0, 1)
+    flipy=random.randint(0, 1)
+    rot=random.randint(0, 1)
+    if flipx==1:
+        img=img[::-1,:,:]
+    if flipy==1:
+        img=img[::,::-1,:]
+    if rot==1:
+        img=np.transpose(img,(1,0,2))
+
+    return img
+
+
+class SingleImageDataset(dataset_mixin.DatasetMixin):
+    def __init__(self, paths, in_size=32, scale=4):
+        self.paths=sorted(glob.glob(join(paths,'*.png')))
+        self.in_size=in_size
+        self.scale=scale
+        self.out_size=in_size*scale
+
+    def __len__(self):
+        return len(self.paths)
+
+    def get_example(self, i): #-> Image:
+        path = self.paths[i]
+        #HR = Image.open(path)
+        HR = scipy.misc.imread(path)
+        HR = randomcrop(HR, size=self.out_size)
+        HR = augumentation(HR)
+
+        h,w,c=HR.shape
+        hr=Image.fromarray(HR)
+        lr=hr.resize((h//self.scale,w//self.scale),Image.BICUBIC)
+
+        LR=np.asarray(lr)
+        LR=np.transpose(LR,(2,0,1))
+        HR=np.transpose(HR,(2,0,1))
+        return LR.astype(np.float32)/255., HR.astype(np.float32)/255.
+        
+class PILImageDataset(dataset_mixin.DatasetMixin):
+    def __init__(self, paths, resize=None, root='.'):
+        if isinstance(paths, six.string_types):
+            with open(paths) as paths_file:
+                paths = [path.strip() for path in paths_file]
+        self._paths = paths
+        self._root = root
+        self._resize = resize
+
+    def __len__(self):
+        return len(self._paths)
+
+    def get_example(self, i):# -> Image:
+        path = os.path.join(self._root, self._paths[i])
+        original_image = Image.open(path)
+        if not self._resize is None:
+            return original_image.resize(self._resize)
+        else:
+            return original_image
+
+
+class ResizedImageDataset(dataset_mixin.DatasetMixin):
+    def __init__(self, paths, resize=None, root='.', dtype=np.float32):
+        self.base = PILImageDataset(paths=paths, resize=resize, root=root)
+        self._dtype = dtype
+
+    def __len__(self):
+        return len(self.base)
+
+    def get_example(self, i):# -> np.ndarray:
+        image = self.base[i]
+        image_ary = np.asarray(image, dtype=self._dtype)
+        if len(image_ary.shape) == 2: # mono
+            image_ary = np.dstack((image_ary, image_ary, image_ary))
+        image_data = image_ary.transpose(2, 0, 1)
+        if image_data.shape[0] == 4:  # RGBA
+            image_data = image_data[:3]
+        return image_data
+
+
+class PreprocessedImageDataset(dataset_mixin.DatasetMixin):
+    def __init__(self, paths, cropsize, resize=None, root='.', dtype=np.float32):
+        self.base = ResizedImageDataset(paths=paths, resize=resize, root=root)
+        self._dtype = dtype
+        self.cropsize = cropsize
+
+    def __len__(self):
+        return len(self.base)
+
+    def get_example(self, i):# -> np.ndarray:
+        image = self.base[i]
+        x = random.randint(0, image.shape[1] - self.cropsize)
+        y = random.randint(0, image.shape[2] - self.cropsize)
+
+        cropeed_high_res = image[:, x:x + self.cropsize, y:y + self.cropsize]
+        cropped_low_res = cv2.resize(cropeed_high_res.transpose(1, 2, 0), dsize=(int(self.cropsize/4), int(self.cropsize/4)),
+                                     interpolation=cv2.INTER_CUBIC).transpose(2, 0, 1)
+        return cropped_low_res, cropeed_high_res
---
+++ main.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+import argparse
+import glob
+import os
+import chainer
+import numpy as np
+import functools
+from chainer.backends import cuda
+
+from dataset import SingleImageDataset
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--datapath", type=str, default='./pictures',
+                                            help='Please prepare HR images in this directory')
+parser.add_argument("--insize", type=int,default=32)
+parser.add_argument("--scale", type=int,default=4)
+parser.add_argument("--gpu", type=int, default=-1)
+parser.add_argument("--batchsize", type=int, default=16)
+parser.add_argument("--pretrained_generator")
+args = parser.parse_args()
+
+if args.gpu >= 0:
+    #chainer.cuda.check_cuda_available()
+    #chainer.cuda.get_device(args.gpu).use()
+    xp = chainer.cuda.cupy
+else:
+    xp = np
+
+
+class default_conv2d(chainer.Link):
+    def __init__(self, in_channels=64, out_channels=64, ksize=3, stride=1, pad=1, activate=chainer.functions.leaky_relu):
+        super(default_conv2d, self).__init__()
+        with self.init_scope():
+            self.conv=chainer.links.Convolution2D(in_channels, out_channels, ksize=ksize, stride=stride, pad=pad)
+            self.activate=activate
+
+    def __call__(self, x):
+        x1=self.conv(x)
+        return self.activate(x1)
+
+class MODEL(chainer.Chain):
+    def __init__(self):
+        super(MODEL, self).__init__()
+        with self.init_scope():
+            self.first=default_conv2d(3, 64, ksize=5, stride=1, pad=2)
+            self.conv1=default_conv2d(64, 64, ksize=3, stride=1, pad=1)
+            self.deconv1=chainer.links.Deconvolution2D(64,3,ksize=4,stride=4)
+
+    def __call__(self, x: chainer.Variable, test=False):
+        
+        x0 = self.first(x)
+
+        x0=self.conv1(x0)
+
+        h=self.deconv1(x0)
+
+        return h
+
+def train():
+    dataset = SingleImageDataset(paths=args.datapath, in_size=args.insize, scale=args.scale)
+
+    iterator = chainer.iterators.MultithreadIterator(dataset, batch_size=args.batchsize, repeat=True, shuffle=True)
+    # iterator = chainer.iterators.SerialIterator(dataset, batch_size=args.batchsize, repeat=True, shuffle=True)
+
+    generator = MODEL()
+    if args.pretrained_generator is not None:
+        chainer.serializers.load_npz(args.pretrained_generator, generator)
+    if args.gpu >= 0:
+        generator.to_gpu(args.gpu)
+
+    optimizer_generator = chainer.optimizers.Adam(alpha=1e-4)
+    optimizer_generator.setup(generator)
+    optimizer_generator.use_cleargrads()
+
+    # updater = chainer.training.updaters.StandardUpdater(iterator, optimizer_generator, device=None)#args.gpu)
+
+    # trainer = chainer.training.Trainer(updater, (200, 'epoch'), out='result')
+    # trainer.run()
+
+    step = 0
+    sum_loss_generator, sum_loss_generator_adversarial, sum_loss_generator_content = 0, 0, 0
+    for zipped_batch in iterator:
+        lr = chainer.Variable(xp.array([zipped[0] for zipped in zipped_batch]))
+        hr = chainer.Variable(xp.array([zipped[1] for zipped in zipped_batch]))
+
+        sr = generator(lr)
+
+        
+        loss_generator = chainer.functions.mean_absolute_error(
+                sr,
+                hr
+        )
+        #optimizer_generator.zero_grads()
+        loss_generator.backward()
+        optimizer_generator.update()
+        loss_g=chainer.cuda.to_cpu(loss_generator.data)
+        sum_loss_generator += loss_g
+
+        report_span = 1
+        save_span = 500
+        step += 1
+        if step % report_span == 0:
+            sum_loss_generator, sum_loss_generator_adversarial, sum_loss_generator_content = 0, 0, 0
+            print("Step: {}".format(step))
+            print("loss_generator: {}".format(loss_g))
+        if step % save_span == 0:
+            chainer.serializers.save_npz(
+                os.path.join('checkpoint', "generator_model_{}.npz".format(step)), generator)
+
+
+if __name__=='__main__':
+    train()
